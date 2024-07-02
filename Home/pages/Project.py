bfe407651a5c2b89144d66e423dfbeb7453e9751
Himaghna2011 <himaghnasingh@gmail.com>
2024-07-01 22:26:32 -0400

    Update Project.py

--- Home/pages/Project.py
+++ Home/pages/Project.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import requests
-
+from streamlit_lottie import st_lottie
 
 st.set_page_config(page_title="My Webpage", page_icon=":tada:")
 
@@ -24,3 +24,7 @@ def load_lottiesurl(url):
     if requ.status_code != 200:
         return None
     return requ.json()
+
+lotties_files = load_lottiesurl("https://lottie.host/82be80b1-8f25-414e-8e8b-ec8639917416/H4sRDOIEgR.json")
+
+st_lottie(lotties_files, height = 300, key = "coding")
