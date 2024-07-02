ce3da41333249938790da7b22c9e4e8b2fa626fe
Himaghna2011 <himaghnasingh@gmail.com>
2024-07-02 12:05:09 -0400

    Update 3_Contact.py

--- Home/pages/3_Contact.py
+++ Home/pages/3_Contact.py
@@ -47,9 +47,8 @@ with st.container():
     st.header("Get In Touch With Me!")
     st.write("##")
 
-    # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
-    contact_form = """
-    <form action="https://formsubmit.co/himaghnasingh@gmail.com" method="POST">
+ contact_form = """
+    <form action="https://formsubmit.co/himaghnasingh@gmail.com,himaghna313@gmail.com" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
